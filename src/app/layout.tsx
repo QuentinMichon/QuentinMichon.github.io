504568aa173511b8f42bc5120d76f6560b032de9
QuentinMichon <quentin.michon@heig-vd.ch>
2025-11-04 15:36:38 +0100

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Bitcount_Grid_Single } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -10,6 +10,11 @@ const geistSans = Geist({
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+});
+
+const bitcountGridSingle = Bitcount_Grid_Single({
+    variable: "--font-bitcount-grid-single",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
@@ -24,7 +29,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} ${bitcountGridSingle.variable}`}>
         {children}
       </body>
     </html>
